de4e954bbcaa51ab7076e161deee8eac5aa13e4d
GERALD JOSE CALVO VIDAURRE <gcalvov0792@uvirtual.ac.cr>
2021-06-27 14:46:53 -0600

    Tablas procedimientos almacenados

---
+++ BaseDatos/dbo/StoreProcedures/TitulosInsertar.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [dbo].[TitulosInsertar]
+	@Descripcion VARCHAR(250),
+	@Estado BIT
+AS
+  BEGIN
+    SET NOCOUNT ON
+	BEGIN TRANSACTION TRASA
+
+	BEGIN TRY
+
+	INSERT INTO Titulos
+	(
+	   Descripcion
+	 , Estado
+	)
+
+	VALUES 
+	(
+	   @Descripcion
+	 , @Estado
+	
+	)
+	
+	COMMIT TRANSACTION TRASA
+
+	  SELECT 0 AS CodeError, '' AS MsgError
+
+	END TRY
+
+	BEGIN CATCH
+
+	  SELECT
+		ERROR_NUMBER() AS CodeError
+	  , ERROR_MESSAGE() AS MsgError
+
+
+	  ROLLBACK TRANSACTION TRASA
+	END CATCH
+
+  END
---
+++ BaseDatos/dbo/StoreProcedures/TitulosObtener.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[TitulosObtener]
+	@Id_Titulo INT= NULL
+
+AS
+	BEGIN
+	SET NOCOUNT ON
+
+	SELECT 
+	  Id_Titulo
+	, Descripcion
+	, Estado
+
+	FROM Titulos
+	WHERE (@Id_Titulo IS NULL OR Id_Titulo=@Id_Titulo)
+
+	END
