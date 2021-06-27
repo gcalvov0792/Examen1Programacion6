79d82a22483616c8f95ea33e8c05a404c8c70b16
GERALD JOSE CALVO VIDAURRE <gcalvov0792@uvirtual.ac.cr>
2021-06-26 22:14:55 -0600

    nuevods

--- WebApp/App_Start/ConfigHttpClient.cs
+++ WebApp/App_Start/ConfigHttpClient.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplicationCore
+namespace WebApp
 {
     public static class ConfigHttpClient
     {
--- WebApp/App_Start/ExtensionHttp.cs
+++ WebApp/App_Start/ExtensionHttp.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace WebApplicationCore
+namespace WebApp
 {
     public static class ExtensionHttp
     {
